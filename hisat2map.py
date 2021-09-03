4bd5e23457082a5e7908ba47f7774b7dabbb6e46
martinuslee <supreme034@korea.ac.kr>
2021-09-03 22:05:05 +0900

    sample option add

--- hisat2map.py
+++ hisat2map.py
@@ -23,36 +23,44 @@ parser = argparse.ArgumentParser(
 # argument enrollment
 parser.add_argument("-t", '--thread', dest='thread', type=str, default='1', action="store",
                     help='Launch NTHREADS parallel build threads')
-parser.add_argument("-p", '--path', dest='index_path', type=str, default='2.HISAT2REF/', action="store",
+parser.add_argument("-i", '--index', dest='index_path', type=str, default='2.HISAT2REF/', action="store",
                     help='Directory path HISAT2 index files stored')
 parser.add_argument("-s", '--sample', dest='sample_path', type=str, default='3.SAMPLE/', action="store",
                     help='Directory path Sample files stored')
-
+parser.add_argument('--per', dest='sample_rate', type=str, default='', action="store",
+                    help='Sample rate ( e.g. sampled10%%, sampled5%%, sampled05%% )')
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 args = parser.parse_args()
 
 f = Figlet(font='slant')
 #print(f.renderText('* * * * * * * * *'))
-print(f.renderText('   HISAT2\nEVERYTHING'))
+print(f.renderText('\n    HISAT2\nEVERYTHING'))
 #print(f.renderText('* * * * * * * * *'))
 
-print(f"Thread : {args.thread}")
-print(f"Index Path : {args.index_path}")
-print(f"Sample Path : {args.sample_path}")
+print(f"---------------------------------------------------------------------------")
+print(f"- Thread : {args.thread}")
+print(f"- Index Path : {args.index_path}")
+print(f"- Sample Path : {args.sample_path}")
+print(f"- Sample % : {args.sample_rate}")
+print(f"---------------------------------------------------------------------------")
 
 ####################################################################################
+
 
 def getMapped(indexFile, f1, f2):
     # indexFile, f1, f2 = args_f  # ref : fasta, species : species name
     try:
-        directory = '4.HISAT2MAP/logs/'
-        if not os.path.exists(directory): # if there is not a directory..
+        directory = '4.HISAT2MAP/' + args.sample_rate + '/logs/'
+        sam_dir = '4.HISAT2MAP/' + args.sample_rate + '/'
+        if not os.path.exists(directory):  # if there is not a directory..
             os.makedirs(directory)
-        basename  = indexFile.split('/')[1]
+        basename = indexFile.split('/')[1]
         cmd = "time hisat2 -p " + args.thread + " --rna-strandness RF -x " + indexFile + \
-            " -1 " + f1 + " -2 " + f2 + " -S 4.HISAT2MAP/" + basename + ".sam 1> " + \
-            directory + basename + ".log 2>> " +  directory + basename + ".log"# HISAT2 command line
-        #os.system(cmd)  #### RUN!! ####
-        #print(cmd) #test
+            " -1 " + f1 + " -2 " + f2 + " -S " + sam_dir + basename + ".sam 1> " + \
+            directory + basename + ".log 2>> " + directory + \
+            basename + ".log"  # HISAT2 command line
+        os.system(cmd)  #### RUN!! ####
+        #print(cmd)  # test
     except OSError:
         print('Error: Creating directory. ' + directory)
 
@@ -67,17 +75,18 @@ def getMapped(indexFile, f1, f2):
 
 if __name__ == '__main__':
 
-    start = time.time()  # 시작 시간 저장
+    start = time.time()  # set timer to check execute time
     pool = Pool(10)  # 10개씩 돌아감
-    dirList = glob(args.index_path + '*/') #get sub directories
-    dirList = [path + path.split('/')[1] for path in dirList] # get sub dir with basename of index files
-    dirList.sort() # sort A-Z
-    
-    fastq1, fastq2 = glob('3.SAMPLE/*.fastq')
+    dirList = glob(args.index_path + '*/')  # get sub directories
+    # get sub dir with basename of index files
+    dirList = [path + path.split('/')[1] for path in dirList]
+    dirList.sort()  # sort A-Z
 
-    pool.starmap(getMapped, [ [item, fastq1, fastq2] for item in dirList])
+    fastq1, fastq2 = glob('3.SAMPLE/2.done/' + args.sample_rate + '*.fastq')
+
+    pool.starmap(getMapped, [[item, fastq1, fastq2] for item in dirList])
     pool.close()
     pool.join()
 
-    print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
-    
+    # current time - start time  = sys time
+    print("time :", time.time() - start)
