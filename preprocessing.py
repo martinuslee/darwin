57a109ce0e05b6032b160f7d60d2840b5f8c36c6
martinuslee <supreme034@korea.ac.kr>
2021-10-20 18:41:04 +0900

    client app

--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,26 @@
 from Bio import SeqIO
 import random
 
-#"3.SAMPLE/SRR1758114_2.fastq"
-def sampling(path, origin1, origin2, per):
-    per = float(per)
-    per = per/100.0 # percentage to float 10% == 0.1
-    records = list(SeqIO.parse(path + origin1,"fastq"))
-    records_2 = list(SeqIO.parse(path + origin2,"fastq"))
-    print(records[0])
-    sam = int(len(records)*per) #0.1%
-    sam
-    index = list(range(1,len(records)+1))
-    print("index length : ",len(index))
-    sampleIdx = random.sample(index, sam)
-    sampleIdx.sort()
-    #sampleRecords
+#len(identifiers)
+species = input() # Coturnix_japonica
+sample = input() # SRR1758114_1.fastq
 
-    sampleRecords=list()
-    sampleRecords_2=list()
-    for i in sampleIdx:
-        sampleRecords.append(records[i])
-        sampleRecords_2.append(records_2[i])
-    per = per * 100
-    int(per) if per > 0 else per
-    per = str(per)
-    per = per.replace(".","")
-    print(path+"sampled"+ per + "%_"+origin1, "fastq")
-    SeqIO.write(sampleRecords, path+"sampled"+ per + "%_"+origin1, "fastq")
-    SeqIO.write(sampleRecords_2, path+"sampled"+ per + "%_"+origin2, "fastq")
+print(species, sample)
 
-sampling('3.SAMPLE/', 'SRR1758114_1.fastq','SRR1758114_2.fastq', 0.00005)
+#File Read
+records , records_2= list(SeqIO.parse(f"3.SAMPLE/{species}/{sample}","fastq")), list(SeqIO.parse(f"3.SAMPLE/{species}/{sample}","fastq"))
+
+index = list(range(1,len(records)+1))
+
+sampleIdx = sorted(random.sample(index, 10))
+
+print(sampleIdx)
+
+sampleRecords, sampleRecords_2 = [], []
+for i in sampleIdx:
+    sampleRecords.append(records[i])
+    sampleRecords_2.append(records_2[i])
+
+# File Write
+SeqIO.write(sampleRecords, f'3.SAMPLE/Coturnix_japonica/test_r10_SRR1758114_1.fastq', "fastq")
+SeqIO.write(sampleRecords_2, f'3.SAMPLE/Coturnix_japonica/test_r10_SRR1758114_2.fastq', "fastq")
