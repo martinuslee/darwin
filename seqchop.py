bc45c0913ab39b577f85d33d8d1e7b05a096d828
martinuslee <supreme034@korea.ac.kr>
2021-10-30 18:22:59 +0900

    client gui

--- seqchop.py
+++ seqchop.py
@@ -1,13 +1,12 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtCore import QBasicTimer
 from Bio import SeqIO
 import random
 import os
 
-form_class = uic.loadUiType("/Users/jongheon/Desktop/10reads.ui")[0]
-
-class MyWindow(QWidget, form_class):
+class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setupUI()
@@ -16,15 +15,15 @@ class MyWindow(QWidget, form_class):
         self.setGeometry(800, 200, 600, 300)
         self.setWindowTitle("Seqchop v0.0.1")
 
-        self.pushButton = QPushButton("File Open")
-        self.pushButton2 = QPushButton("File Open")
+        self.pushButton = QPushButton("Select First FASTQ Read File")
+        self.pushButton2 = QPushButton("Select Second FASTQ Read File")
         self.runButton = QPushButton("Start")
         
         self.pushButton.clicked.connect(self.pushButtonClicked)
         self.pushButton2.clicked.connect(self.pushButtonClicked2)
         self.runButton.clicked.connect(self.runReadWrite)
-        self.label = QLabel()
-        self.label2 = QLabel()
+        self.label = QLabel("Fastq read 1 : ")
+        self.label2 = QLabel("Fastq read 2 : ")
 
         self.label_state = QLabel("Ready..!")
 
@@ -38,8 +37,6 @@ class MyWindow(QWidget, form_class):
 
         self.setLayout(layout)
 
-        
-
     def pushButtonClicked(self):
         fname = QFileDialog.getOpenFileName(self)
         self.label.setText(fname[0])
@@ -51,32 +48,41 @@ class MyWindow(QWidget, form_class):
         self.label_state.setText("Ready..!")  
 
     def runReadWrite(self):
+        try:
+            self.label_state.setText("File Reading...")
+            
+            # File Read
+            fastq1 = self.label.text()
+            fastq2 = self.label2.text()
+            
+            records, records_2 = list(SeqIO.parse(fastq1, "fastq")), list(
+                SeqIO.parse(fastq2, "fastq"))
+            index = list(range(1, len(records)+1))
+            
+            sampleIdx = list()
 
-        self.label_state.setText("File Reading...")
-        #File Read
-        #print(self.label.text(), self.label2.text())
-        fastq1 = self.label.text()
-        fastq2 = self.label2.text()
-        
-        records , records_2= list(SeqIO.parse(fastq1,"fastq")), list(SeqIO.parse(fastq2,"fastq"))
-        index = list(range(1,len(records)+1))
+            sampleIdx = sorted(random.sample(index, 10))
 
-        sampleIdx = sorted(random.sample(index, 10))
-        
-        print(sampleIdx)
-        
-        sampleRecords, sampleRecords_2 = [], []
-        print(os.path.dirname(fastq1) + "r10" + os.path.basename(fastq1))
+            print(sampleIdx)
 
-        for i in sampleIdx:
-            sampleRecords.append(records[i])
-            sampleRecords_2.append(records_2[i])
-        # File Write
-        
-        self.label_state.setText("File Writting...")
-        SeqIO.write(sampleRecords, os.path.dirname(fastq1) + "/r10" + os.path.basename(fastq1), "fastq")
-        SeqIO.write(sampleRecords_2, os.path.dirname(fastq2) + "/r10" + os.path.basename(fastq2), "fastq")
-        self.label_state.setText("Done..! Path : " + os.path.dirname(fastq1))
+            sampleRecords, sampleRecords_2 = [], []
+            print(os.path.dirname(fastq1) + "r10" + os.path.basename(fastq1))
+
+            for i in sampleIdx:
+                sampleRecords.append(records[i])
+                sampleRecords_2.append(records_2[i])
+            # File Write
+
+            self.label_state.setText("File Writting...")
+            SeqIO.write(sampleRecords, os.path.dirname(fastq1) +
+                        "/r10" + os.path.basename(fastq1), "fastq")
+            SeqIO.write(sampleRecords_2, os.path.dirname(fastq2) +
+                        "/r10" + os.path.basename(fastq2), "fastq")
+            self.label_state.setText(
+                "Done..! \n Check : " + os.path.dirname(fastq1))
+        except FileNotFoundError:
+            self.label_state.setText("Check your FASTQ File...")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
