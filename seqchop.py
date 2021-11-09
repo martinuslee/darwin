7a610541d019e5f6cdcfa29a64facda0337c43dc
martinuslee <supreme034@korea.ac.kr>
2021-11-09 16:46:12 +0900

    seqchop v.0.0.1

--- seqchop.py
+++ seqchop.py
@@ -1,11 +1,55 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from PyQt5.QtCore import QBasicTimer
+from PyQt5.QtCore import *
 from Bio import SeqIO
 import random
 import os
 
+class ThreadClass(QThread):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+    def run(self):
+        try:
+            self.parent.label_state.setText("File Reading...")
+
+            # File Read
+            fastq1 = self.parent.label.text()
+            fastq2 = self.parent.label2.text()
+            data = open(fastq1, 'rb')
+
+            data.seek(-2, os.SEEK_END)
+            while data.read(1) != b' ':
+                data.seek(-2, os.SEEK_CUR)
+            while data.read(1) != b'.':
+                data.seek(-2, os.SEEK_CUR)
+            length = int(data.readline().decode().split(' ')[0])
+            print(length)
+            index = list(range(1,length+1))
+            sampleIdx = sorted(random.sample(index, 10))
+
+            record_dict, record2_dict= SeqIO.index(fastq1, "fastq"), SeqIO.index(fastq2, "fastq")
+
+            #print(sampleIdx)
+            sampleName = os.path.basename(fastq1).split('_')[0]
+            sampleRecords, sampleRecords_2 = [], []
+            for idx in sampleIdx:
+                sampleRecords.append(record_dict[f'{sampleName}.{idx}'])
+                sampleRecords_2.append(record2_dict[f'{sampleName}.{idx}'])
+
+            print(os.path.dirname(fastq1) + "/r10" + os.path.basename(fastq1))
+            # File Write
+
+            self.parent.label_state.setText("File Writting...")
+            SeqIO.write(sampleRecords, os.path.dirname(fastq1) +
+                        "/r10" + os.path.basename(fastq1), "fastq")
+            SeqIO.write(sampleRecords_2, os.path.dirname(fastq2) +
+                        "/r10" + os.path.basename(fastq2), "fastq")
+            self.parent.label_state.setText(
+                "Done..! \n Check : " + os.path.dirname(fastq1))
+        except FileNotFoundError:
+            self.parent.label_state.setText("Check your FASTQ File...")
 
 class MyWindow(QWidget):
     def __init__(self):
@@ -40,55 +84,17 @@ class MyWindow(QWidget):
 
     def pushButtonClicked(self):
         fname = QFileDialog.getOpenFileName(self)
-        self.label.setText(fname[0])
+        self.label.setText("Fastq read 1 : " + fname[0])
         self.label_state.setText("Ready..!")
 
     def pushButtonClicked2(self):
         fname = QFileDialog.getOpenFileName(self)
-        self.label2.setText(fname[0])
+        self.label2.setText("Fastq read 2 : " + fname[0])
         self.label_state.setText("Ready..!")
 
     def runReadWrite(self):
-        try:
-            self.label_state.setText("File Reading...")
-
-            # File Read
-            fastq1 = self.label.text()
-            fastq2 = self.label2.text()
-            data = open(fastq1, 'rb')
-
-            data.seek(-2, os.SEEK_END)
-            while data.read(1) != b' ':
-                data.seek(-2, os.SEEK_CUR)
-            while data.read(1) != b'.':
-                data.seek(-2, os.SEEK_CUR)
-            length = int(data.readline().decode().split(' ')[0])
-            print(length)
-            index = list(range(1,length+1))
-            sampleIdx = sorted(random.sample(index, 10))
-
-            record_dict, record2_dict= SeqIO.index(fastq1, "fastq"), SeqIO.index(fastq2, "fastq")
-
-            #print(sampleIdx)
-            sampleName = fastq1.split('_')[0]
-            sampleRecords, sampleRecords_2 = [], []
-            for idx in sampleIdx:
-                sampleRecords.append(record_dict[f'{sampleName}.{idx}'])
-                sampleRecords_2.append(record2_dict[f'{sampleName}.{idx}'])
-
-            print(os.path.dirname(fastq1) + "r10" + os.path.basename(fastq1))
-            # File Write
-
-            self.label_state.setText("File Writting...")
-            SeqIO.write(sampleRecords, os.path.dirname(fastq1) +
-                        "/r10" + os.path.basename(fastq1), "fastq")
-            SeqIO.write(sampleRecords_2, os.path.dirname(fastq2) +
-                        "/r10" + os.path.basename(fastq2), "fastq")
-            self.label_state.setText(
-                "Done..! \n Check : " + os.path.dirname(fastq1))
-        except FileNotFoundError:
-            self.label_state.setText("Check your FASTQ File...")
-
+        x = ThreadClass(self)
+        x.start()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
